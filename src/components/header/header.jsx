64d44a51ae1aab7498e73414272282a33f62194d
Korede <oyeyemikorede5@gmail.com>
2024-07-18 12:23:41 +0100

    Mobile view update

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import Logo from "../../assets/Logo 2.png";
 import { menuLinks } from "../../utils/data";
@@ -7,16 +7,31 @@ import Cart from "../../svg/cart";
 import Notification from "../../svg/notification";
 import UserIcon from "../../svg/user";
 import DropDown from "../../svg/drop";
+import Mobile from "../../svg/mobile";
 
 const Header = () => {
+  const [mobile, setMobile] = useState(false);
   return (
-    <div className="headerContainer">
+    <div className={mobile ? "headerContainer headMobile" : "headerContainer"}>
       <div className="headerContain">
         <div className="logo">
           <img src={Logo} alt="logo" />
+          <div className="mobileBtn">
+            <FormSearch />
+            <div onClick={() => setMobile(!mobile)}>
+              <Mobile />
+            </div>
+          </div>
         </div>
         <div className="otherMenus">
           <div className="menuLinks">
+            <div className="mobilehid">
+              <div className="userIcon">
+                <UserIcon />
+                <p>Christopher John</p>
+                <DropDown />
+              </div>
+            </div>
             {menuLinks?.map((item, index) => {
               return <p key={index}>{item}</p>;
             })}
---
+++ src/svg/mobile.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Mobile = () => {
+  return (
+    <svg
+      width="32"
+      height="33"
+      viewBox="0 0 32 33"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4 24.9492H28"
+        stroke="#066FE0"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M4 16.9492H28"
+        stroke="#066FE0"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M4 8.94922H28"
+        stroke="#066FE0"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Mobile;
